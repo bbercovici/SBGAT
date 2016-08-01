fde0d4cfcbd5763552bd9e007e0373d4791542d4
Benjamin Bercovici <bbercovici@rgnt1-56-18-dhcp.int.colorado.edu>
2016-08-01 13:51:57 -0600

    added the documentation

---
+++ doc/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['getrenderer',['getRenderer',['../class_main_window.html#a2fb384b2232488ced24aad4237e0290e',1,'MainWindow']]]
+];
---
+++ doc/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mainwindow',['MainWindow',['../class_main_window.html#a34c4b4207b46d11a4100c9b19f0e81bb',1,'MainWindow']]]
+];
