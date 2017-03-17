76d64361105d422f78280f0c94ed6adda5d855ef
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-03-16 18:16:38 -0600

    ready to test Golevka

--- SBGAT_core/source/main.cpp
+++ SBGAT_core/source/main.cpp
@@ -15,16 +15,14 @@ int main( int argc, char** argv ) {
     
 
 	ShapeModel shape_model;
-	ShapeModelImporter shape_io("../../resources/earth_spherical.obj");
+	ShapeModelImporter shape_io("../../resources/KW4Alpha.obj");
 	shape_io.load_shape_model(&shape_model);
 
 	DynamicAnalyses dynamic_analyses(&shape_model);
-	
-	// dynamic_analyses.compute_pgm(Density::KW4_ALPHA_DENSITY);
-	arma::vec p = {6378000,0,0};
     start = std::chrono::system_clock::now();
 	
-	std::cout << dynamic_analyses.pgm_acceleration(p,Density::EARTH_DENSITY);
+	dynamic_analyses.compute_pgm(Density::KW4_ALPHA_DENSITY);
+	
 
     end = std::chrono::system_clock::now();
  	std::chrono::duration<double> elapsed_seconds = end-start;
