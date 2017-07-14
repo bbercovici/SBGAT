19101e9da4d9de59a9c759da96b1f88a8701718b
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-07-14 16:51:01 -0600

    removed old folders

--- SBGAT_core/SBGATCoreConfig.cmake
+++
@@ -1,3 +0,0 @@
-message("SBGAT_CORE found")
-set(SBGAT_CORE_INCLUDE_HEADER /Users/bbercovici/GDrive/CUBoulder/Research/code/SBGAT/SBGAT_core/include)
-set(SBGAT_CORE_LIBRARY /Users/bbercovici/GDrive/CUBoulder/Research/code/SBGAT/SBGAT_core/lib)
