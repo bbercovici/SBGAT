bf9d1274fa2aacb82acaf55169ac4d53dddc66a3
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-04-18 16:46:01 -0600

    commit before pre-release

--- SbgatCore/SbgatCore/CMake/SbgatCoreConfig.cmake
+++ SbgatCore/SbgatCore/CMake/SbgatCoreConfig.cmake
@@ -20,29 +20,14 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-if (EXISTS /home/bebe0705/.am_fortuna)
-	set(IS_FORTUNA ON)
-	message("-- This is Fortuna")
+set(SBGATCORE_INCLUDE_HEADER /usr/local/include/SbgatCore/)
 
+if (APPLE)
+	set(SBGATCORE_LIBRARY /usr/local/lib/libSbgatCore.dylib)
+elseif(UNIX AND NOT APPLE)
+	set(SBGATCORE_LIBRARY /usr/local/lib/libSbgatCore.so)
 else()
-	set(IS_FORTUNA OFF)
-endif()
-
-
-if(${IS_FORTUNA})
-	set(SBGATCORE_INCLUDE_HEADER /home/bebe0705/libs/local/include/SbgatCore/)
-	set(SBGATCORE_LIBRARY /home/bebe0705/libs/local/lib/libSbgatCore.so)
-
-else()
-	set(SBGATCORE_INCLUDE_HEADER /usr/local/include/SbgatCore/)
-
-	if (APPLE)
-		set(SBGATCORE_LIBRARY /usr/local/lib/libSbgatCore.dylib)
-	elseif(UNIX AND NOT APPLE)
-		set(SBGATCORE_LIBRARY /usr/local/lib/libSbgatCore.so)
-	else()
-		message(FATAL_ERROR "Unsupported platform")
-	endif()
+	message(FATAL_ERROR "Unsupported platform")
 endif()
 
 
