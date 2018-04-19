fa39417bb04c2e8ff21cad7a731c93ffa487c729
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-04-19 10:56:37 -0600

    updated configuration file

--- SbgatCore/SbgatCore/CMake/SbgatCoreConfig.cmake
+++ SbgatCore/SbgatCore/CMake/SbgatCoreConfig.cmake
@@ -20,30 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-if (EXISTS /home/bebe0705/.am_fortuna)
-	set(IS_FORTUNA ON)
-	message("-- This is Fortuna")
-
-else()
-	set(IS_FORTUNA OFF)
-endif()
-
-
-if(${IS_FORTUNA})
-	set(SBGATCORE_INCLUDE_HEADER /home/bebe0705/libs/local/include/SbgatCore/)
-	set(SBGATCORE_LIBRARY /home/bebe0705/libs/local/lib/libSbgatCore.so)
-
-else()
-	set(SBGATCORE_INCLUDE_HEADER /usr/local/include/SbgatCore/)
-
-	if (APPLE)
-		set(SBGATCORE_LIBRARY /usr/local/lib/libSbgatCore.dylib)
-	elseif(UNIX AND NOT APPLE)
-		set(SBGATCORE_LIBRARY /usr/local/lib/libSbgatCore.so)
-	else()
-		message(FATAL_ERROR "Unsupported platform")
-	endif()
-endif()
-
+set(SBGATCORE_INCLUDE_HEADER /home/bebe0705/libs/local/include/SbgatCore/)
+set(SBGATCORE_LIBRARY /home/bebe0705/libs/local/lib/libSbgatCore.so)
 
 message("-- Found SbgatCore: " ${SBGATCORE_LIBRARY})
