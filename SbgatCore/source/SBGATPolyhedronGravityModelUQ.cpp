5d7979b97ccff31c82db4688495ccacf25c16fc3
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-27 23:35:00 -0700

    in progress

--- SbgatCore/source/SBGATPolyhedronGravityModelUQ.cpp
+++ SbgatCore/source/SBGATPolyhedronGravityModelUQ.cpp
@@ -534,7 +534,7 @@ void SBGATPolyhedronGravityModelUQ::TestPartialUfPartialTf(std::string filename,
 		shape_uq.SetPGM(pgm_filter);
 
 		int N_facets = vtkPolyData::SafeDownCast(pgm_filter -> GetInput()) -> GetNumberOfCells();
-		int f = arma::randi(arma::distr_param(0,N_facets - 1));
+		int f = arma::randi<arma::vec>(arma::distr_param(0,N_facets - 1))(0);
 
 		arma::vec pos = {2,3,4};
 
