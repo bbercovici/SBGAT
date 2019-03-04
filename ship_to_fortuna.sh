c33721d0c9fd96837ebe27b524fc8d7a3a03192f
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-03-04 14:27:40 -0700

    unbroke cmakes

---
+++ ship_to_fortuna.sh
@@ -0,0 +1,14 @@
+git checkout develop && 
+git add SbgatCore/include && 
+git add SbgatCore/source && 
+git add Examples/ && 
+git commit -m "shipping to fortuna" &&
+git push origin develop &&
+git checkout fortuna && 
+git checkout develop SbgatCore/include && 
+git checkout develop SbgatCore/source && 
+git checkout develop Examples/*.py &&
+git checkout develop Examples/*.cpp &&
+git commit -m "shipping to fortuna" &&
+git push origin fortuna &&
+git checkout develop
