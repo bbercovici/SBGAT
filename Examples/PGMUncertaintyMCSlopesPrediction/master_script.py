6026f95ee3fc042e10a76cab84252842607d10c4
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-04-24 22:03:03 +0200

    added scripts

--- Examples/PGMUncertaintyMCSlopesPrediction/master_script.py
+++ Examples/PGMUncertaintyMCSlopesPrediction/master_script.py
@@ -25,68 +25,68 @@ def generate_all_cases_dictionnary_list(base_dictionnary,all_cases_dictionnary,b
 
     return all_cases_dictionnary_list
 
+def start_sim():
+    # Replace the paths after 'base_location' with the existing directory under which the input/ and /output sub-directories
+    # will be created and populated
+    if (socket.gethostname() == "fortuna"):
+        base_location = "/orc_raid/bebe0705/PGMUncertaintyMCSlopesPrediction/"
+    else:
+        base_location = "../"
 
-# Replace the paths after 'base_location' with the existing directory under which the input/ and /output sub-directories
-# will be created and populated
-if (socket.gethostname() == "fortuna"):
-    base_location = "/orc_raid/bebe0705/PGMUncertaintyMCSlopesPrediction/"
-else:
-    base_location = "../"
 
+    # SIM_PREFIX will be added to the name of every folder to be put in input/ and output/ 
+    SIM_PREFIX = "PGMUncertaintyMCSlopesPrediction"
 
-# SIM_PREFIX will be added to the name of every folder to be put in input/ and output/ 
-SIM_PREFIX = "PGMUncertaintyMCSlopesPrediction"
+    # Dictionnary storing simulation inputs to be kept constant
+    base_dictionnary = {
+    "DENSITY" : 4500,
+    "UNIT_IN_METERS" : False,
+    "PERIOD" : 4.195948 * 3600,
+    "PERIOD_SD" : 1e-5 * 4.195948 * 3600,
+    "N_MONTE_CARLO" : 1000,
+    "PATH_SHAPE" : "../../../resources/shape_models/psyche.obj",
+    "CORRELATION_DISTANCE" : 75e3,
+    "ERROR_STANDARD_DEV" : 10e3,
+    "COV_REGION_CENTERS" : [0,1147],
+    "UNCERTAINTY_TYPE" : "normal",
+    }
 
-# Dictionnary storing simulation inputs to be kept constant
-base_dictionnary = {
-"DENSITY" : 4500,
-"UNIT_IN_METERS" : False,
-"PERIOD" : 4.195948 * 3600,
-"PERIOD_SD" : 1e-5 * 4.195948 * 3600,
-"N_MONTE_CARLO" : 1000,
-"PATH_SHAPE" : "../../../resources/shape_models/psyche.obj",
-"CORRELATION_DISTANCE" : 75e3,
-"ERROR_STANDARD_DEV" : 10e3,
-"COV_REGION_CENTERS" : [0,1147],
-"UNCERTAINTY_TYPE" : "normal",
-}
+    # Dictionnary storing simulation inputs to be looped over
+    # for instance, one could have
+    # all_cases_dictionnary = {
+    # "INPUT_THAT_MUST_BE_CHANGED_1" : [1,2,3]
+    # "INPUT_THAT_MUST_BE_CHANGED_2" : [True,False]
+    # }
+    # which means that a total of six (two times three) simulations will be run
+    # and saved in input/ and output/, with the names of the subfolder prefixed by SIM_PREFIX"
 
-# Dictionnary storing simulation inputs to be looped over
-# for instance, one could have
-# all_cases_dictionnary = {
-# "INPUT_THAT_MUST_BE_CHANGED_1" : [1,2,3]
-# "INPUT_THAT_MUST_BE_CHANGED_2" : [True,False]
-# }
-# which means that a total of six (two times three) simulations will be run
-# and saved in input/ and output/, with the names of the subfolder prefixed by SIM_PREFIX"
+    all_cases_dictionnary = {
+    "HOLD_MASS_CONSTANT" : [True,False]
+    }
 
-all_cases_dictionnary = {
-"HOLD_MASS_CONSTANT" : [True,False]
-}
+    # There shouldn't be any reason to modify the following
+    all_data = generate_all_cases_dictionnary_list(base_dictionnary,
+    all_cases_dictionnary,base_location,SIM_PREFIX)
+    os.system("cmake .. && make")
+    for data in all_data:
 
-# There shouldn't be any reason to modify the following
-all_data = generate_all_cases_dictionnary_list(base_dictionnary,
-all_cases_dictionnary,base_location,SIM_PREFIX)
-os.system("cmake .. && make")
-for data in all_data:
+        print("\t Case " + data["INPUT_DIR"].split("/")[-2])
+        print("\t - Making directory")
 
-    print("\t Case " + data["INPUT_DIR"].split("/")[-2])
-    print("\t - Making directory")
+        os.system("mkdir " + data["INPUT_DIR"])
+        os.system("mkdir " + data["OUTPUT_DIR"])
 
-    os.system("mkdir " + data["INPUT_DIR"])
-    os.system("mkdir " + data["OUTPUT_DIR"])
+        print("\t - Copying input file in build/")
 
-    print("\t - Copying input file in build/")
-
-    with open('input_file.json', 'w') as outfile:
-        json.dump(data, outfile)
-    print("\t - Saving input file in input/ and output/")
-    with open(data["INPUT_DIR"] + 'input_file.json', 'w') as outfile:
-        json.dump(data, outfile)
-    with open(data["OUTPUT_DIR"] + 'input_file.json', 'w') as outfile:
-        json.dump(data, outfile)
-    print("\t - Running case " +  data["INPUT_DIR"].split("/")[-2])
-    os.system("> " + data["OUTPUT_DIR"] + "log.txt")
-    os.system("./PGMUncertaintyMCSlopesPrediction 2>&1 | tee -a " + data["OUTPUT_DIR"] + "log.txt" )
+        with open('input_file.json', 'w') as outfile:
+            json.dump(data, outfile)
+        print("\t - Saving input file in input/ and output/")
+        with open(data["INPUT_DIR"] + 'input_file.json', 'w') as outfile:
+            json.dump(data, outfile)
+        with open(data["OUTPUT_DIR"] + 'input_file.json', 'w') as outfile:
+            json.dump(data, outfile)
+        print("\t - Running case " +  data["INPUT_DIR"].split("/")[-2])
+        os.system("> " + data["OUTPUT_DIR"] + "log.txt")
+        os.system("./PGMUncertaintyMCSlopesPrediction 2>&1 | tee -a " + data["OUTPUT_DIR"] + "log.txt" )
    
 
