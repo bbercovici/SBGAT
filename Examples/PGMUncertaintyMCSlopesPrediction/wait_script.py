2bb465a5702608593ccc5d9bb9c0994e73f95b24
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-04-24 22:04:23 +0200

    updated wait script

---
+++ Examples/PGMUncertaintyMCSlopesPrediction/wait_script.py
@@ -0,0 +1,14 @@
+import psutil 
+import time 
+import subprocess
+import sys
+sys.path.append("..")
+import master_script
+pid = 27590 
+while(1): 
+    if not psutil.pid_exists(pid): 
+        master_script.start_sim()
+        break
+    else: 
+    	print("PID 27590 is still running")
+        time.sleep(5) 
